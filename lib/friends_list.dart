2cc189d4bf304d11699fb00aeb0960a6ddf8903d
sanchit48 <sanchitkhare3@gmail.com>
2020-03-28 14:29:15 +0530

    friends_list.dart: Refractored code

--- lib/friends_list.dart
+++ lib/friends_list.dart
@@ -1,98 +1,52 @@
 import 'package:chat_app/dummy_friends.dart';
 import 'package:flutter/material.dart';
 import './friends_list.dart';
+import './main_chat.dart';
 
 class FriendsList extends StatelessWidget {
+  // go to main chat
+  void friendChat(int index, BuildContext context) {
+    String key = dummyFriends[index]['key'];
+
+    Navigator.of(context).pushNamed(MainChat.routeName, arguments: key);
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return ListView.builder(
-
       itemBuilder: (context, index) {
-
-        return
-
-          //elevation: 5,
-          //  shape: RoundedRectangleBorder(
-
-          //     borderRadius: BorderRadius.circular(15.0),
-
-          //   ),
-
-
-          Column(
-
-            children: <Widget>[
-              ListTile(
-
-                leading: Container(
-
-                  height: 50,
-                  width: 50,
-
-                  decoration: new BoxDecoration(
-
-                    shape: BoxShape.circle,
-
-                    image: DecorationImage(
-
-                      fit: BoxFit.cover,
-                      image: AssetImage(
-
-                        dummyFriends[index]['pic'],
-
-                      )
-
-                    )
-
-                  ),
-
-                ),
-
-                title: Text(dummyFriends[index]['name']),
-                subtitle: Text(
-
-                  dummyFriends[index]['chat'],
-                  overflow: TextOverflow.ellipsis
-
-                ),
-                trailing: Container(
-
-                    margin: EdgeInsets.only(top: 15),
-                    height: 15,
-                    width: 15,
-                    decoration: BoxDecoration(
-
-                        color: Color(0xffF26060),
-                        shape: BoxShape.circle,
-                        //borderRadius: BorderRadius.circular(15)
-
-                    ),
-
-                    child: FittedBox(
-
-                      child: Text('2', style: TextStyle(color: Colors.white, fontSize: 40),)
-
-                    ),
-
-
-                  ),
-
+        return Column(
+          children: <Widget>[
+            ListTile(
+              onTap: () => friendChat(index, context),
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(dummyFriends[index]['pic']),
+                radius: 25,
               ),
-
-              Divider(),
-
-            ],
-
-          );
-
+              title: Text(dummyFriends[index]['name']),
+              subtitle: Text(dummyFriends[index]['chat'],
+                  overflow: TextOverflow.ellipsis),
+              trailing: Container(
+                margin: EdgeInsets.only(top: 15),
+                height: 15,
+                width: 15,
+                decoration: BoxDecoration(
+                  color: Color(0xffF26060),
+                  shape: BoxShape.circle,
+                  //borderRadius: BorderRadius.circular(15)
+                ),
+                child: FittedBox(
+                    child: Text(
+                  '2',
+                  style: TextStyle(color: Colors.white, fontSize: 40),
+                )),
+              ),
+            ),
+            Divider(),
+          ],
+        );
       },
-
       itemCount: dummyFriends.length,
-
     );
-
   }
-
 }
