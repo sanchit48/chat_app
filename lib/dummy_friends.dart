f71840822dca146ddd2a7f44a3304e525f09fced
sanchit48 <sanchitkhare3@gmail.com>
2020-03-28 14:27:59 +0530

    dummy_friends.dart: Added dummy chat list

--- lib/dummy_friends.dart
+++ lib/dummy_friends.dart
@@ -1,25 +1,30 @@
 var dummyFriends = [
 
-  {'name': 'Abhinav', 'pic': './assets/images/L.jpg', 'chat': 'Hello friends how are you'},
+  {'name': 'Abhinav', 'pic': './assets/images/L.jpg', 'chat': 'Hello friend how are you', 'key': 'a1'},
 
-  {'name': 'Luckyron', 'pic': './assets/images/L.jpg', 'chat': 'Hello friends how are you'},
+  {'name': 'Luckyron', 'pic': './assets/images/L.jpg', 'chat': 'Hello friend how are you', 'key': 'a2'},
 
-  {'name': 'Abhinayy', 'pic': './assets/images/L.jpg','chat': 'Hello friends how are you'},
+  {'name': 'Abhinayy', 'pic': './assets/images/L.jpg','chat': 'Hello friend how are you', 'key': 'a3'},
 
-  {'name': 'Daniyal', 'pic': './assets/images/L.jpg','chat': 'Hello friends how are you,'},
+  {'name': 'Daniyal', 'pic': './assets/images/L.jpg','chat': 'Hello friend how are you,', 'key': 'a4'},
 
-   {'name': 'Abhinav', 'pic': './assets/images/L.jpg', 'chat': 'Hello friends how are you Hello friends how are you Hello friends how are youHello friends how are you'},
+  {'name': 'Karandeep', 'pic': './assets/images/L.jpg', 'chat': 'Hello friend how are you Hello friend How are you', 'key': 'a5'},
 
-  {'name': 'Luckyron', 'pic': './assets/images/L.jpg', 'chat': 'Hello friends how are you'},
+  {'name': 'Toshit', 'pic': './assets/images/L.jpg', 'chat': 'Hello friend how are you', 'key': 'a6'},
 
-  {'name': 'Abhinayy', 'pic': './assets/images/L.jpg','chat': 'Hello friends how are you'},
+  {'name': 'Zubair', 'pic': './assets/images/L.jpg','chat': 'Hello friend how are you', 'key': 'a7'},
 
-  {'name': 'Daniyal', 'pic': './assets/images/L.jpg','chat': 'Hello friends how are you,'},
+  {'name': 'Muzzafar', 'pic': './assets/images/L.jpg','chat': 'Hello friend how are you,', 'key': 'a8'},
 
-  {'name': 'Luckyron', 'pic': './assets/images/L.jpg', 'chat': 'Hello friends how are you'},
 
-  {'name': 'Abhinayy', 'pic': './assets/images/L.jpg','chat': 'Hello friends how are you'},
+];
 
-  {'name': 'Daniyal', 'pic': './assets/images/L.jpg','chat': 'Hello friends how are you,'},
+var dummyChat = [
+
+  {
+    'Abhinav': ['Hey buddy what\'s going on?', 'Ohh that\'s great, how is everyone at home'],
+    'Sanchit': ['Yeah buddy I am fine.', 'Everyone is fine with god\'s grace']
+  }
+
 
 ];
